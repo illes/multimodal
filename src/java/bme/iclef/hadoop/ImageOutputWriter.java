f74875b8dddd6aa716bd2328e2c3842f1ef24c81
Hadooooop <hadoop@fum1.(none)>
2011-10-13 16:25:35 +0200

    Write the vectors to sequence files The sequence file's name is always the file's name, i.e. the supplied key

--- src/java/bme/iclef/hadoop/ImageOutputWriter.java
+++ src/java/bme/iclef/hadoop/ImageOutputWriter.java
@@ -1,22 +1,37 @@
 package bme.iclef.hadoop;
 
 import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataInput;
 
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.RecordWriter;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.lib.MultipleOutputFormat;
 import org.apache.hadoop.util.Progressable;
 
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
+import org.apache.mahout.utils.vectors.io.SequenceFileVectorWriter;
+
 @SuppressWarnings("deprecation")
 public class ImageOutputWriter<K,V> extends MultipleOutputFormat<K,V> {
 
+	static final char keyIndexSeparator = ':'; 
+
 	static protected class ImageRecordWriter<K,V> implements RecordWriter<K,V> {
 
-		public ImageRecordWriter () {
+		private SequenceFile.Writer out;
 
+		public ImageRecordWriter (SequenceFile.Writer writer) {
+			out = writer;
 		}
 
 		public synchronized void write(K key, V value) throws IOException {
@@ -27,15 +42,41 @@ public class ImageOutputWriter<K,V> extends MultipleOutputFormat<K,V> {
 				return;
 			}
 
-			System.out.println ("been called with key: " + key.getClass ());
+			/* deserialize value */
+		        DataInputStream in = new DataInputStream (new ByteArrayInputStream (.getBytes()));	
+			Vector v = VectorWritable.readVector (in);
+
+			/* TODO: it's possible to do the appending without deserialization
+			 * SequenceFile.Writer API supports it... */
+			out.append (key, v);
 
 		}
 
 		public synchronized void close(Reporter reporter) throws IOException {
-
+			out.close ();
 		}
 	}
 
+	@Override
+	protected String generateFileNameForKeyValue (K key, V value, String name) {
+		/* use the key as filename */
+		if (key.getClass () == Text.class) {
+			Text k = (Text) key;
+			String fName = k.toString ();
+
+		        /* handle multiple keys for one file */ 	
+			int sepPos = -1;
+			if ((sepPos = fName.lastIndexOf (keyIndexSeparator)) != -1) {
+				/* remove key index suffix */
+				fName = fName.substring (0, sepPos);
+			}
+
+			return fName;
+		} 
+
+
+		return super.generateFileNameForKeyValue (key, value, name);
+	}
 
 	@Override
 	public RecordWriter<K,V> getBaseRecordWriter (FileSystem fs,
@@ -44,7 +85,11 @@ public class ImageOutputWriter<K,V> extends MultipleOutputFormat<K,V> {
 						  Progressable arg3) throws IOException
 	{
 
-		return new ImageRecordWriter<K,V> ();
+		Path fileOut = MultipleOutputFormat.getTaskOutputPath (job, name);
+		FileSystem fileSys = fileOut.getFileSystem(job);
+		SequenceFile.Writer writer = SequenceFile.createWriter (fileSys, job, fileOut, LongWritable.class, VectorWritable.class);
+
+		return new ImageRecordWriter<K,V> (writer);
 	
 	}
 };
