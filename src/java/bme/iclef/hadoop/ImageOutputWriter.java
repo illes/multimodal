525a4ca2e827b71239082d19449ea355b70bf2aa
Illes Solt <illes.solt@gmail.com>
2011-10-13 16:33:43 +0200

    non-generic sequence file writer

--- src/java/bme/iclef/hadoop/ImageOutputWriter.java
+++ src/java/bme/iclef/hadoop/ImageOutputWriter.java
@@ -1,96 +1,85 @@
 package bme.iclef.hadoop;
 
 import java.io.IOException;
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.DataInput;
 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.RecordWriter;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.lib.MultipleOutputFormat;
 import org.apache.hadoop.util.Progressable;
-
-import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.NamedVector;
 import org.apache.mahout.math.VectorWritable;
-import org.apache.mahout.utils.vectors.io.SequenceFileVectorWriter;
 
 @SuppressWarnings("deprecation")
-public class ImageOutputWriter<K,V> extends MultipleOutputFormat<K,V> {
+public class ImageOutputWriter extends
+	MultipleOutputFormat<Text, VectorWritable> {
 
-	static final char keyIndexSeparator = ':'; 
+    static final char keyIndexSeparator = ':';
 
-	static protected class ImageRecordWriter<K,V> implements RecordWriter<K,V> {
+    static protected class ImageRecordWriter implements
+	    RecordWriter<Text, VectorWritable> {
 
-		private SequenceFile.Writer out;
+	private final SequenceFile.Writer out;
 
-		public ImageRecordWriter (SequenceFile.Writer writer) {
-			out = writer;
-		}
-
-		public synchronized void write(K key, V value) throws IOException {
-			boolean nullKey = key == null || key instanceof NullWritable;
-			boolean nullValue = value == null || value instanceof NullWritable;
-
-			if (nullKey && nullValue) {
-				return;
-			}
-
-			/* deserialize value */
-		        DataInputStream in = new DataInputStream (new ByteArrayInputStream (.getBytes()));	
-			Vector v = VectorWritable.readVector (in);
-
-			/* TODO: it's possible to do the appending without deserialization
-			 * SequenceFile.Writer API supports it... */
-			out.append (key, v);
-
-		}
-
-		public synchronized void close(Reporter reporter) throws IOException {
-			out.close ();
-		}
+	public ImageRecordWriter(SequenceFile.Writer writer) {
+	    out = writer;
 	}
 
-	@Override
-	protected String generateFileNameForKeyValue (K key, V value, String name) {
-		/* use the key as filename */
-		if (key.getClass () == Text.class) {
-			Text k = (Text) key;
-			String fName = k.toString ();
+	public synchronized void write(Text key, VectorWritable value)
+		throws IOException {
+	    boolean nullKey = key == null;
+	    boolean nullValue = value == null;
 
-		        /* handle multiple keys for one file */ 	
-			int sepPos = -1;
-			if ((sepPos = fName.lastIndexOf (keyIndexSeparator)) != -1) {
-				/* remove key index suffix */
-				fName = fName.substring (0, sepPos);
-			}
+	    if (nullKey && nullValue) {
+		return;
+	    }
 
-			return fName;
-		} 
-
-
-		return super.generateFileNameForKeyValue (key, value, name);
+	    // out.append(key, value);
+	    // Wrap the vector into a named vector with the key as name.
+	    out.append(key, new VectorWritable(new NamedVector(value.get(),
+		    key.toString())));
 	}
 
-	@Override
-	public RecordWriter<K,V> getBaseRecordWriter (FileSystem fs,
-						  JobConf job,
-						  String name,
-						  Progressable arg3) throws IOException
-	{
-
-		Path fileOut = MultipleOutputFormat.getTaskOutputPath (job, name);
-		FileSystem fileSys = fileOut.getFileSystem(job);
-		SequenceFile.Writer writer = SequenceFile.createWriter (fileSys, job, fileOut, LongWritable.class, VectorWritable.class);
-
-		return new ImageRecordWriter<K,V> (writer);
-	
+	public synchronized void close(Reporter reporter) throws IOException {
+	    out.close();
 	}
+    }
+
+    @Override
+    protected String generateFileNameForKeyValue(Text key,
+	    VectorWritable value, String name) {
+	/* use the key as filename */
+	if (key.getClass() == Text.class) {
+	    Text k = (Text) key;
+	    String fName = k.toString();
+
+	    /* handle multiple keys for one file */
+	    int sepPos = -1;
+	    if ((sepPos = fName.lastIndexOf(keyIndexSeparator)) != -1) {
+		/* remove key index suffix */
+		fName = fName.substring(0, sepPos);
+	    }
+
+	    return fName;
+	}
+
+	return super.generateFileNameForKeyValue(key, value, name);
+    }
+
+    @Override
+    public RecordWriter<Text, VectorWritable> getBaseRecordWriter(
+	    FileSystem fs, JobConf job, String name, Progressable arg3)
+	    throws IOException {
+	Path fileOut = MultipleOutputFormat.getTaskOutputPath(job, name);
+	FileSystem fileSys = fileOut.getFileSystem(job);
+	SequenceFile.Writer writer = SequenceFile.createWriter(fileSys, job,
+		fileOut, LongWritable.class, VectorWritable.class);
+
+	return new ImageRecordWriter(writer);
+    }
 };
-
